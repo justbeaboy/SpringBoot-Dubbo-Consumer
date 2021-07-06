5a07fa7232e80e23bf35cd9d632a97a6a898beac
Mr.SoftRock <zhangyb13@asiainfo.com>
2021-07-06 15:41:16 +0800

    update a lot

---
+++ src/main/java/com/nacos/test/config/CanalClient.java
@@ -0,0 +1,207 @@
+package com.nacos.test.config;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.alibaba.otter.canal.protocol.Message;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.alibaba.otter.canal.protocol.CanalEntry.*;
+
+
+/**
+ * @author Mr.SoftRock
+ * @Date 2021/7/6 15:28
+ **/
+@Component
+public class CanalClient implements InitializingBean {
+
+    //sql队列
+    private Queue<String> SQL_QUEUE = new ConcurrentLinkedQueue<>();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //第一步：与canal进行连接
+        CanalConnector connector = CanalConnectors.newSingleConnector(new InetSocketAddress("localhost",
+                11111), "example", "", "");
+        int batchSize = 1000;
+        try {
+            connector.connect();
+            //第二步:开启订阅
+            connector.subscribe(".*\\..*");
+            connector.rollback();
+            try {
+                //第三步：循环订阅
+                while (true) {
+                    //尝试从master那边拉去数据batchSize条记录，有多少取多少
+                    //每次读取1000条
+                    Message message = connector.getWithoutAck(batchSize);
+                    long batchId = message.getId();
+                    int size = message.getEntries().size();
+                    if (batchId == -1 || size == 0) {
+                        Thread.sleep(1000);
+                    } else {
+                        dataHandle(message.getEntries());
+                    }
+                    connector.ack(batchId);
+
+//                    当队列里面堆积的sql大于一定数值的时候就模拟执行
+                    if (SQL_QUEUE.size() >= 1) {
+                        executeQueueSql();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            connector.disconnect();
+        }
+    }
+
+
+    /**
+     * 模拟执行队列里面的sql语句
+     */
+    public void executeQueueSql() {
+        int size = SQL_QUEUE.size();
+        for (int i = 0; i < size; i++) {
+            String sql = SQL_QUEUE.poll();
+            System.out.println("[sql]----> " + sql);
+
+
+        }
+    }
+
+    /**
+     * 数据处理
+     *
+     * @param entrys
+     */
+    private void dataHandle(List<CanalEntry.Entry> entrys) throws InvalidProtocolBufferException {
+        for (CanalEntry.Entry entry : entrys) {
+            // step1:拆解entry实体
+            CanalEntry.Header header = entry.getHeader();
+            CanalEntry.EntryType entryType = entry.getEntryType();
+
+            if (CanalEntry.EntryType.ROWDATA == entryType) {
+                CanalEntry.RowChange rowChange = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+                CanalEntry.EventType eventType = rowChange.getEventType();
+                if (eventType == CanalEntry.EventType.QUERY || rowChange.getIsDdl()) {
+                    System.out.println("执行了查询语句：[{}]" + rowChange.getSql());
+                } else if (eventType == CanalEntry.EventType.DELETE) {
+                    saveDeleteSql(entry);
+                } else if (eventType == CanalEntry.EventType.UPDATE) {
+                    saveUpdateSql(entry);
+                } else if (eventType == CanalEntry.EventType.INSERT) {
+                    saveInsertSql(entry);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 保存更新语句
+     *
+     * @param entry
+     */
+    private void saveUpdateSql(CanalEntry.Entry entry) {
+        try {
+            RowChange rowChange = RowChange.parseFrom(entry.getStoreValue());
+            List<RowData> rowDatasList = rowChange.getRowDatasList();
+            for (RowData rowData : rowDatasList) {
+                List<CanalEntry.Column> newColumnList = rowData.getAfterColumnsList();
+                StringBuffer sql = new StringBuffer("update " + entry.getHeader().getTableName() + " set ");
+                for (int i = 0; i < newColumnList.size(); i++) {
+                    sql.append(" " + newColumnList.get(i).getName()
+                            + " = '" + newColumnList.get(i).getValue() + "'");
+                    if (i != newColumnList.size() - 1) {
+                        sql.append(",");
+                    }
+                }
+                sql.append(" where ");
+                List<CanalEntry.Column> oldColumnList = rowData.getBeforeColumnsList();
+                for (CanalEntry.Column column : oldColumnList) {
+                    if (column.getIsKey()) {
+                        //暂时只支持单一主键
+                        sql.append(column.getName() + "=" + column.getValue());
+                        break;
+                    }
+                }
+                SQL_QUEUE.add(sql.toString());
+            }
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 保存删除语句
+     *
+     * @param entry
+     */
+    private void saveDeleteSql(CanalEntry.Entry entry) {
+        try {
+            RowChange rowChange = RowChange.parseFrom(entry.getStoreValue());
+            List<RowData> rowDatasList = rowChange.getRowDatasList();
+            for (RowData rowData : rowDatasList) {
+                List<CanalEntry.Column> columnList = rowData.getBeforeColumnsList();
+                StringBuffer sql = new StringBuffer("delete from " + entry.getHeader().getTableName() + " where ");
+                for (CanalEntry.Column column : columnList) {
+                    if (column.getIsKey()) {
+                        //暂时只支持单一主键
+                        sql.append(column.getName() + "=" + column.getValue());
+                        break;
+                    }
+                }
+                SQL_QUEUE.add(sql.toString());
+            }
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 保存插入语句
+     *
+     * @param entry
+     */
+    private void saveInsertSql(CanalEntry.Entry entry) {
+        try {
+            CanalEntry.RowChange rowChange = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+            List<RowData> rowDatasList = rowChange.getRowDatasList();
+            for (RowData rowData : rowDatasList) {
+                List<CanalEntry.Column> columnList = rowData.getAfterColumnsList();
+                StringBuffer sql = new StringBuffer("insert into " + entry.getHeader().getTableName() + " (");
+                for (int i = 0; i < columnList.size(); i++) {
+                    sql.append(columnList.get(i).getName());
+                    if (i != columnList.size() - 1) {
+                        sql.append(",");
+                    }
+                }
+                sql.append(") VALUES (");
+                for (int i = 0; i < columnList.size(); i++) {
+                    sql.append("'" + columnList.get(i).getValue() + "'");
+                    if (i != columnList.size() - 1) {
+                        sql.append(",");
+                    }
+                }
+                sql.append(")");
+                SQL_QUEUE.add(sql.toString());
+            }
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+    }
+}
